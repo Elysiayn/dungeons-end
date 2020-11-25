5112646364864f936ccb4bc5071d4a44d679a84a
Clu <retsulcconnor@gmail.com>
2020-11-25 18:31:04 -0500

    poll test

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
 var inputEl = document.getElementById("player-name");
-// var buttonEl = document.getElementById("start-button");
+
 
 var beginAdventure = function(event) {
     console.log("Hooray!");
