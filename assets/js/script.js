e00e1f4938b7bd7b60f28767b8194a1c1c93f76e
Clu <retsulcconnor@gmail.com>
2020-11-25 18:11:30 -0500

    Added eventListener to start game

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+var inputEl = document.getElementById("player-name");
+// var buttonEl = document.getElementById("start-button");
+
+var beginAdventure = function(event) {
+    console.log("Hooray!");
+};
+
+document.getElementById("start-button").addEventListener("click", beginAdventure);
