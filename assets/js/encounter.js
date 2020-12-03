b38e48703419614fec4e42be9468850530eb84b6
Da'Vonta Williamson <69822943+wdavonta@users.noreply.github.com>
2020-12-02 21:37:53 -0500

    update

--- assets/js/encounter.js
+++ assets/js/encounter.js
@@ -260,6 +260,7 @@ document.addEventListener('DOMContentLoaded', function() {
  /*  $(document).ready(function(){
     $('#healthportions').modal();
   }); */ 
+  
 
  document.getElementById("attack-button").addEventListener("click", hitDiceRoll);
  document.getElementById("dodge-button").addEventListener("click", playerDodge);
