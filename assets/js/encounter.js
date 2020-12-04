44cdf02a720dea3b7a450bb88429cefd11a10732
Da'Vonta Williamson <69822943+wdavonta@users.noreply.github.com>
2020-12-03 20:21:57 -0500

    update modal so it can work

--- assets/js/encounter.js
+++ assets/js/encounter.js
@@ -306,9 +306,9 @@ document.addEventListener('DOMContentLoaded', function() {
   });
 
 // Health Portion Modal //
- /*  $(document).ready(function(){
-    $('#healthportions').modal();
-  }); */ 
+$(document).ready(function(){
+    $('.modal').modal();
+  })
   
 
 
